5bb75d99d6624f7db9a49fdc411993edfe430096
Thorsten Wißmann <edu@thorsten-wissmann.de>
2017-08-31 21:44:48 +0200

    Sort reminders

--- Caligraph/Backend.hs
+++ Caligraph/Backend.hs
@@ -5,6 +5,7 @@ import Data.Time.Calendar (Day)
 import Data.Time.Calendar (Day,addDays,diffDays)
 
 import Text.Printf
+import Data.Ord
 import Data.Array
 import qualified Data.List as List
 
@@ -14,11 +15,28 @@ data Incarnation = Incarnation
   , duration :: Maybe (Int,Int)
   , title :: String
   -- , TODO: a timezone
-  } deriving (Eq,Show,Ord)
+  } deriving (Eq,Show)
 
 showTime :: (Int,Int) -> String
 showTime (h,m) =
     printf "%2d:%02d" h m
+
+instance Ord Incarnation where
+  compare i1 i2 =
+    either id (const EQ) $ do
+      -- sorting is basically lexicographic
+      compare_on day i1 i2
+      -- but we want Just x to be smaller than Nothing
+      compare_on (Down . fmap Down . time) i1 i2
+      compare_on duration i1 i2
+      compare_on title i1 i2
+    where compare_on projection v1 v2
+            =
+            case compare (projection v1) (projection v2) of
+            EQ -> Right ()
+            LT -> Left LT
+            GT -> Left GT
+
 
 data Item = Item
   { lifetime :: (Maybe Day, Maybe Day)
@@ -55,6 +73,9 @@ inLifetime d range = lifetimeIntersects (d,d) range
 query :: Backend -> (Day,Day) -> Array Day [Incarnation]
 query be (from,to)
     = accumArray (flip (:)) [] (from,to)
+    -- we need to sort in reverse order,
+    -- because we prepend via (:) in accumArray
+    $ List.sortBy (comparing Down)
     $ map (\x -> (day x, x))
     $ filter (\i -> (day i) `inLifetime` (Just from, Just to))
     $ concatMap (\i -> incarnations i from to)
