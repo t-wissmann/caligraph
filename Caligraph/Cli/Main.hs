0bff38c20a1dc578a10ab5113bb4c5ad6682f3d3
Thorsten Wißmann <edu@thorsten-wissmann.de>
2017-12-11 11:37:14 +0100

    Draft of the Possibly monad transformer

--- Caligraph/Cli/Main.hs
+++ Caligraph/Cli/Main.hs
@@ -43,6 +43,30 @@ import Lens.Micro.Mtl
 
 type St =  Caligraph.Cli.AppState.AppState
 
+-- Either a pure or monadic computation
+data Possibly m a = Pure a | Monadic (m a)
+
+instance Functor m => Functor (Possibly m) where
+    fmap f (Pure x) = Pure (f x)
+    fmap f (Monadic mx) = Monadic $ fmap f mx
+
+instance Applicative m => Applicative (Possibly m) where
+    pure = Pure
+    Pure f <*> Pure x = Pure (f x)
+    Pure f <*> Monadic x = Monadic $ pure f <*> x
+    Monadic f <*> Pure x = Monadic $ f <*> pure x
+    Monadic f <*> Monadic x = Monadic $ f <*> x
+
+instance Monad m => Monad (Possibly m) where
+    Pure x >>= f = f x
+    Monadic mx >>= f = Monadic $ do
+        x <- mx
+        case f x of
+            Pure x -> return x
+            Monadic x -> x
+
+data Cmd st = StateT st (Possibly IO ())
+
 binds :: Map.Map ([Modifier],Key) (St -> EventM WidgetName (Next St))
 binds = Map.fromList
   [ (([], KEsc), halt)
