34588a3ea843b6b90490c73a5e51549be7ecc53e
Thorsten Wißmann <edu@thorsten-wissmann.de>
2019-07-23 09:42:52 +0200

    Implement command binding

--- Caligraph/Config/Command.hs
+++ Caligraph/Config/Command.hs
@@ -6,14 +6,16 @@
 module Caligraph.Config.Command where
 
 import Control.Monad (ap)
+import Text.Read (readEither)
+import Text.Printf (printf)
 
 data CommandArgParser target
     = NoMoreArguments target
-    | MandatoryArgument (String -> CommandArgParser target)
+    | MandatoryArgument (String -> Either String (CommandArgParser target))
 
 instance Functor CommandArgParser where
     fmap f (NoMoreArguments p) = NoMoreArguments $ f p
-    fmap f (MandatoryArgument p) = MandatoryArgument $ fmap (fmap f) p
+    fmap f (MandatoryArgument p) = MandatoryArgument $ fmap (fmap (fmap f)) p
 
 instance Applicative CommandArgParser where
     pure = NoMoreArguments
@@ -23,11 +25,30 @@ instance Monad CommandArgParser where
     return = pure
     (NoMoreArguments a) >>= a_to_mb = a_to_mb a
     (MandatoryArgument f) >>= a_to_mb =
-        MandatoryArgument (\s -> (f s) >>= a_to_mb)
+        MandatoryArgument (\s -> do
+            l <- (f s)
+            return (l >>= a_to_mb))
 
 readArg :: Read a => CommandArgParser a
-readArg = MandatoryArgument (return . read)
+readArg = MandatoryArgument (\s -> fmap return $ readEither s)
 
+
+-- | bind and parse the arguments to obtain sth of target type
+bind :: CommandArgParser target -> [String] -> Either String target
+bind = bindCount 0
+  where
+    bindCount :: Int -> CommandArgParser target -> [String] -> Either String target
+    bindCount n (NoMoreArguments t) [] = return t
+    bindCount n (NoMoreArguments _) l =
+        Left
+          $ printf "Too many arguments given (expected %d, given %d)"
+            n (n + length l)
+    bindCount n (MandatoryArgument _) [] =
+        Left
+          $ printf "Too few arguments given (given %d, expected more)" n
+    bindCount n (MandatoryArgument clos) (x:xs) = do
+        nextParser <- clos x
+        bindCount (n+1) nextParser xs
 
 -- to test suite
 noArgs :: IO ()
