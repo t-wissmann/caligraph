c5584fbef82feea910c3b0d3304697719b8edc52
Thorsten Wißmann <edu@thorsten-wissmann.de>
2019-08-21 17:16:03 +0200

    Prepare option parsing

--- app/Main.hs
+++ app/Main.hs
@@ -8,6 +8,13 @@ import Data.Either
 import Control.Monad
 import System.Environment
 
+import Options.Applicative
+import Data.Semigroup ((<>))
+
+data CliOpts = CliOpts
+  { calendarfFile :: FilePath
+  }
+
 mainOld :: IO ()
 mainOld = do
   args <- getArgs
@@ -17,4 +24,20 @@ mainOld = do
                  print e
     Right r -> print r
 
-main = CliM.testmain
+optparse :: Parser CliOpts
+optparse = CliOpts
+  <$> strOption
+    ( long "file"
+    <> metavar "FILE"
+    <> short 'f'
+    <> value ""
+    <> help "Calendar file to open (instead of configured calendars)" )
+
+main :: IO ()
+main = do
+  -- res <- execParser opts
+  CliM.testmain
+  where
+    opts = info (optparse <**> helper)
+      ( fullDesc
+      <> progDesc "Caligraph - calendar interactive and graphical")
