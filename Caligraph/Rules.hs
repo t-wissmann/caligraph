d9b7a9da334c06f810337d65f243e1b1e670b349
Thorsten Wißmann <edu@thorsten-wissmann.de>
2019-09-07 12:24:00 +0200

    Combine condition and consequence parsers

--- Caligraph/Rules.hs
+++ Caligraph/Rules.hs
@@ -1,12 +1,17 @@
 module Caligraph.Rules where
 
 import Caligraph.Config.Types
+import Caligraph.Config.Main (getSource)
 
 import qualified Data.HashMap.Strict as M
 import Data.Text (Text)
 import qualified Data.Text as T
 import Graphics.Vty.Attributes
 import Text.Read
+import Data.Either (partitionEithers)
+import Data.List (intersperse)
+import Control.Monad (unless)
+import System.Environment.XDG.BaseDir
 
 -- | a rule applies of all conditions match, and if this is the case,
 -- then all the consequences are executed
@@ -25,20 +30,44 @@ data Consequence
   | ItemColor UiColor
   | ItemColorInv UiColor
 
-parseCondition :: (Text,Text) -> Either String Condition
-parseCondition (key,value) = do
-  f <- M.lookup (T.unpack key) name2condition `errorName` "no such condition"
-  f <$> (readEither $ T.unpack value)
+parseConditionConsequence :: (Text,Text) -> Either String (Either Condition Consequence)
+parseConditionConsequence (key,value) = do
+  f <- M.lookup (T.unpack key) name2conditionOrConsequence
+        `errorName` ("no such condition or consequence: " ++ T.unpack key)
+  f (T.unpack value)
   where
-    name2condition = M.fromList
+    name2conditionOrConsequence :: M.HashMap String (String -> Either String (Either Condition Consequence))
+    name2conditionOrConsequence = M.fromList $
+      mapSnds (\constr -> Right . Left . constr) name2condition
+      ++ mapSnds ((.) $ fmap Right) name2consequence
+
+    mapSnds :: (a -> b) -> [(x,a)] -> [(x,b)]
+    mapSnds f = map (\(x,a) -> (x, f a))
+
+    name2condition :: [(String,String -> Condition)]
+    name2condition =
       [ (,) "calendar" CalendarName
       , (,) "description" DescriptionRegex
       ]
+    name2consequence :: [(String,String -> Either String Consequence)]
+    name2consequence =
+      [ c "visible"   ItemVisible
+      , c "color"     ItemColor
+      , c "color-inv" ItemColorInv
+      ]
+    c :: UserReadShow x => String -> (x -> Consequence) -> (String, String -> Either String Consequence)
+    c name constructor = (name, \s -> constructor <$> userRead s)
 
 errorName :: Maybe a -> String -> Either String a
 errorName (Just x) _ = Right x
 errorName Nothing s = Left s
 
--- parseRule :: SectionParser Rule
--- parseRule section =
---   M.toList section
+parseRule :: SectionParser Rule
+parseRule section = do
+  let (ls,rs) = partitionEithers $ map parseConditionConsequence (M.toList section)
+  unless (null ls) $ -- if ls is not empty
+    -- then fail with all error messages present
+    Left $ concat $ intersperse ['\n'] ls
+  let (condits,conseqs) = partitionEithers rs
+  return (Rule condits conseqs)
+
