5df2c28a6c1ae3cfa033b60c7683a2e77811be7c
Thorsten Wißmann <edu@thorsten-wissmann.de>
2020-01-30 07:08:05 +0100

    Draft for parameter parsing

--- Caligraph/IcsFile/Parser.hs
+++ Caligraph/IcsFile/Parser.hs
@@ -6,6 +6,8 @@ import Text.ParserCombinators.Parsec hiding (parse)
 import Text.ParserCombinators.Parsec.Token
 import qualified Text.ParserCombinators.Parsec as P
 
+import Data.Char
+
 foo = "bar"
 
 type IcsLine = (Int,String)
@@ -22,17 +24,39 @@ parseLines = flip sepEndBy (many1 $ char '\n') $ do
     return $ (pos, (c:s) ++ concat indentLines)
 
 -- | a param is: name = value[, …]
-type Param = (String, [String])
+type Param = (String, String, [String])
 type ContentLine = (String, [Param], String)
 
+-- | parse param-name, here we do not distinguish between iana-token
+-- and x-name
 parseName :: GenParser Char st String
 parseName = many1 $ oneOf $ ['0'..'9'] ++ "-" ++ ['a'..'z'] ++ ['A'..'Z']
 
+-- | parse a param
+-- param         = param-name "=" param-value *("," param-value)
 parseParam :: GenParser Char st Param
-parseParam = error "sdf"
+parseParam = (,,) <$> parseName
+                  <*> parseValue
+                  <*> many (sepBy (char ',') parseValue)
 
 parseValue :: GenParser Char st String
-parseValue = error "not yet defined"
+parseValue = (char '"' >> many qsafeChar <* char '"')
+    <|> many safeChar
+    where
+        -- QSAFE-CHAR    = WSP / %x21 / %x23-7E / NON-US-ASCII
+        -- ; Any character except CONTROL and DQUOTE
+        qsafeChar :: GenParser Char st Char
+        qsafeChar = noneOf (control ++ "\"")
+        --  SAFE-CHAR     = WSP / %x21 / %x23-2B / %x2D-39 / %x3C-7E / NON-US-ASCII
+        -- ; Any character except CONTROL, DQUOTE, ";", ":", ","
+        safeChar :: GenParser Char st Char
+        safeChar = noneOf (control ++ "\";:,")
+
+        -- CONTROL       = %x00-08 / %x0A-1F / %x7F
+        -- ; All the controls except HTAB
+        control :: [Char]
+        control = map chr $ [0x00..0x08] ++ [0x0A..0x1F] ++ [0x7F]
+
 
 
 parseContentLine :: GenParser Char st ContentLine
