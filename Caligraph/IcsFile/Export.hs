798df4d5de9fad4ace192afa9f7fd34eb8d90c18
Thorsten Wißmann <edu@thorsten-wissmann.de>
2023-09-01 20:46:05 +0200

    Basic ics export

--- Caligraph/IcsFile/Export.hs
+++ Caligraph/IcsFile/Export.hs
@@ -9,6 +9,7 @@ import qualified Data.Text.Lazy as T
 import qualified Data.Map.Strict as M
 import qualified Data.Set as S
 import Data.Time.Clock
+import Data.Time.LocalTime
 
 import Text.ICalendar.Printer
 import Text.ICalendar.Types
@@ -28,7 +29,10 @@ print meta incs = printICalendar def $ VCalendar
     , vcMethod = Nothing
     , vcOther = S.empty
     , vcTimeZones = M.empty
-    , vcEvents = M.fromList (map extract_vevent_keys $ map (convert_incarnation meta) incs)
+    , vcEvents = M.fromList $ do
+        (index,inc) <- zip [0..] incs
+        return $ extract_vevent_keys
+               $ convert_incarnation meta index inc
     , vcTodos = M.empty
     , vcJournals = M.empty
     , vcFreeBusys = M.empty
@@ -38,12 +42,17 @@ print meta incs = printICalendar def $ VCalendar
 extract_vevent_keys :: VEvent -> ((T.Text, Maybe (Either Date DateTime)), VEvent)
 extract_vevent_keys event = ((uidValue $ veUID event, Nothing), event)
 
-convert_incarnation :: Metadata -> CB.Incarnation () -> VEvent
-convert_incarnation meta inc = VEvent
+convert_incarnation :: Metadata -> Int -> CB.Incarnation () -> VEvent
+convert_incarnation meta index inc = VEvent
     { veDTStamp       = DTStamp (currentTime meta) def -- FIXME
     , veUID           = UID (T.pack uid) def -- FIXME
     , veClass         = def :: Class -- ^ 'def' = 'Public'
-    , veDTStart       = Nothing
+    , veDTStart       = Just $
+        case (CB.time inc) of
+        Nothing ->
+            DTStartDate (Date $ CB.day inc) def
+        Just (h,m) ->
+            DTStartDateTime (FloatingDateTime $ LocalTime (CB.day inc) (TimeOfDay h m 0)) def
     , veCreated       = Nothing
     , veDescription   = Nothing
     , veGeo           = Nothing
@@ -58,7 +67,10 @@ convert_incarnation meta inc = VEvent
     , veUrl           = Nothing
     , veRecurId       = Nothing
     , veRRule         = S.empty
-    , veDTEndDuration = Nothing
+    , veDTEndDuration =
+        case (CB.duration inc) of
+        Nothing -> Nothing
+        Just (h,m) -> Just (Right $ DurationProp (DurationTime def h m 0) def)
     , veAttach        = S.empty
     , veAttendee      = S.empty
     , veCategories    = S.empty
@@ -73,4 +85,4 @@ convert_incarnation meta inc = VEvent
     , veOther         = S.empty
     }
     where
-        uid = "test"
+        uid = "item" ++ show index
